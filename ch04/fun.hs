994270653bd9811071a3dbc6da4a1210d87ae044
danhlutz <danhlutz@gmail.com>
2018-07-19 18:14:58 -0400

    Swap Integer for Int in FIB

--- ch04/fun.hs
+++ ch04/fun.hs
@@ -1,9 +1,9 @@
 --fun.hs
 module Fib where
 
-fib :: Int -> Int
+fib :: Integer -> Integer
 fib n = helper 1 0 n
 
-helper :: Int -> Int -> Int -> Int
+helper :: Integer -> Integer -> Integer -> Integer
 helper b a 0 = a
 helper b a n = helper (b + a) b (n - 1)
