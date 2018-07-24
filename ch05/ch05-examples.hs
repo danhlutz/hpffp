8cbd83b2d5c9721462a1370ed2a86f8faef46f95
danhlutz <danhlutz@gmail.com>
2018-07-24 08:10:05 -0400

    Explore currying

---
+++ ch05/ch05-examples.hs
@@ -0,0 +1,36 @@
+--ch05-examples.hs
+module Examples where
+
+addStuff :: Integer -> Integer -> Integer
+addStuff a b = a + b + 5
+
+subtractStuff :: Integer
+              -> Integer
+              -> Integer
+subtractStuff x y = x - y - 10
+
+subtractOne = subtractStuff 1
+
+nonsense :: Bool -> Integer
+nonsense True = 805
+nonsense False = 31337
+
+curriedFunction :: Integer
+                -> Bool
+                -> Integer
+curriedFunction i b = 
+  i + (nonsense b)
+
+uncurriedFunction :: (Integer, Bool)
+                  -> Integer
+uncurriedFunction (i, b) = 
+  i + (nonsense b)
+
+anonymous :: Integer -> Bool -> Integer
+anonymous = \i b-> i + (nonsense b)
+
+anonNested :: Integer
+           -> Bool
+           -> Integer
+anonNested = 
+  \i -> \b -> i + (nonsense b)
