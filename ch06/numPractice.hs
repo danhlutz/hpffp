4f7d8d4779ac8c5ffd912b1ed5f9b0038516450f
danhlutz <danhlutz@gmail.com>
2018-08-06 08:15:23 -0400

    Practice messing up type signatures

---
+++ ch06/numPractice.hs
@@ -0,0 +1,10 @@
+--numPractice.hs
+--practice in Num section
+module NumPractice where
+
+divideThenAdd :: Fractional a
+              => a -> a -> a
+divideThenAdd x y = (x / y) + 1
+
+subtractThenAdd :: Num a => a -> a -> a
+subtractThenAdd x y = (x - y) + 1
