fa7277908fd1ff3a16e43a7b53fa710a3f5a8143
danhlutz <danhlutz@gmail.com>
2018-09-12 08:19:34 -0400

    Type practice

--- ch11/ch11-01.hs
+++ ch11/ch11-01.hs
@@ -9,3 +9,23 @@ data Doggies a =
 data DogueDeBordeaux doge =
   DogueDeBordeaux doge
   deriving Show
+
+data Price = 
+  Price Integer
+  deriving (Eq, Show)
+
+data Manufacturer =
+    Mini
+  | Mazda
+  | Tata
+  deriving (Eq, Show)
+
+data Airline =
+    PapuAir
+  | CatapultsR'Us
+  | TakeYourChancesUnited
+  deriving (Eq, Show)
+
+data Vehicle = Car Manufacturer price
+             | Plane Airline
+             deriving (Eq, Show)
