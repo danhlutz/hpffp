bfe1f6dca3c1123127fc4a511fc40cfa61f5edac
danhlutz <danhlutz@gmail.com>
2018-09-26 18:16:17 -0400

    Process strings

---
+++ ch12/ex02.hs
@@ -0,0 +1,42 @@
+-- ex02.hs
+module Exercises where
+
+import Data.List (intersperse)
+
+-- 1)
+notThe :: String -> Maybe String
+notThe "the" = Nothing
+notThe x     = Just x
+
+replaceThe :: String -> String
+replaceThe str = concat $ intersperse " " (helper (words str))
+
+helper :: [String] -> [String]
+helper [] = []
+helper (w:ws)
+  | notThe w == Nothing = "a" : helper ws
+  | otherwise           = w : helper ws
+
+-- 2)
+vowelInitiated :: String -> Bool
+vowelInitiated (x:xs) = elem x "aeiou"
+
+countHelp :: [String] -> Integer
+countHelp [] = 0
+countHelp (x:[]) = 0
+countHelp (a:b:bs)
+  | a == "the" && (vowelInitiated b) = 1 + countHelp (b:bs)
+  | otherwise = countHelp (b:bs)
+
+countTheBeforeVowel :: String -> Integer
+countTheBeforeVowel str = countHelp (words str)
+
+-- 3)
+countVowels :: String -> Integer
+countVowels [] = 0
+countVowels (w:ws)
+  | isVowel w = 1 + countVowels ws
+  | otherwise = countVowels ws
+
+isVowel :: Char -> Bool
+isVowel x = elem x "aAeEiIoOuU"
