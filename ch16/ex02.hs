3155acab522396cd8b1b47085afbfadb59992446
danhlutz <danhlutz@gmail.com>
2018-10-26 18:11:17 -0400

    Write good and bad fmaps

---
+++ ch16/ex02.hs
@@ -0,0 +1,12 @@
+-- ch16/ex02.hs
+
+module Exercise where
+
+data FixMePls a =
+    FixMe
+  | Pls a
+  deriving (Eq, Show)
+
+instance Functor FixMePls where
+  fmap _ FixMe = FixMe
+  fmap f (Pls a) = Pls (f a)
