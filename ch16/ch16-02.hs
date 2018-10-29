905c25902ea171572e9a8ea620501ccf980f174b
danhlutz <danhlutz@gmail.com>
2018-10-29 08:15:57 -0400

    Compose fmaps

---
+++ ch16/ch16-02.hs
@@ -0,0 +1,57 @@
+-- ch16/ch16-02.hs
+module Practice where
+
+main :: IO ()
+main = do
+  putStr "replaceWithP' lms: "
+  print(replaceWithP' lms)
+
+  putStr "liftedReplace lms: "
+  print (liftedReplace lms)
+
+  putStr "liftedReplace' lms: "
+  print (liftedReplace' lms)
+
+  putStr "twiceLifted lms: "
+  print (twiceLifted lms)
+
+  putStr "twiceLifted' lms: "
+  print (twiceLifted' lms)
+
+  putStr "thriceLifted lms: "
+  print (thriceLifted lms)
+
+  putStr "thriceLifted' lms: "
+  print (thriceLifted' lms)
+
+
+--
+
+replaceWithP :: b -> Char
+replaceWithP = const 'p'
+
+lms :: [Maybe [Char]]
+lms = [Just "Ave", Nothing, Just "woohoo"]
+
+replaceWithP' :: [Maybe [Char]] -> Char
+replaceWithP' = replaceWithP
+
+liftedReplace :: Functor f => f a -> f Char
+liftedReplace = fmap replaceWithP
+
+liftedReplace' :: [Maybe [Char]] -> [Char]
+liftedReplace' = liftedReplace
+
+twiceLifted :: (Functor f1, Functor f) =>
+               f (f1 a) -> f (f1 Char)
+twiceLifted = (fmap . fmap) replaceWithP
+
+twiceLifted' :: [Maybe [Char]] -> [Maybe Char]
+twiceLifted' = twiceLifted
+
+thriceLifted :: (Functor f1, Functor f2, Functor f3)
+             => f1 (f2 (f3 a)) -> f1 (f2 (f3 Char))
+thriceLifted = (fmap . fmap . fmap) replaceWithP
+
+thriceLifted' :: [Maybe [Char]] -> [Maybe [Char]]
+thriceLifted' = thriceLifted
