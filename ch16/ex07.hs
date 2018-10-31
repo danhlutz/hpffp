fbc13c5505490cecdec448c536ed9483c66da044
danhlutz <danhlutz@gmail.com>
2018-10-31 08:07:34 -0400

    Make a functor for an either-like type

---
+++ ch16/ex07.hs
@@ -0,0 +1,12 @@
+-- ch16/ex07.hs
+
+module Exercise where
+
+data Sum a b =
+    First a
+  | Second b
+  deriving (Eq, Show)
+
+instance Functor (Sum a) where
+  fmap _ (First x) = First x
+  fmap f (Second y) = Second (f y)
