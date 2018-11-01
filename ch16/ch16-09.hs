a7a6e65f7b8aef1397487a07fe9806f795ec96f7
danhlutz <danhlutz@gmail.com>
2018-11-01 17:37:18 -0400

    Use a Functor in a type declaration

---
+++ ch16/ch16-09.hs
@@ -0,0 +1,10 @@
+-- ch16/ch16-09.hs
+
+module Practice where
+
+data Wrap f a =
+  Wrap (f a)
+  deriving (Eq, Show)
+
+instance Functor f => Functor (Wrap f) where
+  fmap f (Wrap fa) = Wrap (fmap f fa)
