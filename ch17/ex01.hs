93c82c19c5e5419c8af95f76431009406a94e564
danhlutz <danhlutz@gmail.com>
2018-11-06 18:03:03 -0500

    Practice using applicatives

---
+++ ch17/ex01.hs
@@ -0,0 +1,51 @@
+-- ch17/ex01.hs
+
+module Exercise where
+
+import Data.List (elemIndex)
+
+-- 1
+
+added :: Maybe Integer
+added = (+3) <$> (lookup 3 $ zip [1, 2, 3] [4, 5, 6])
+
+-- 2
+
+y :: Maybe Integer
+y = lookup 3 $ zip [1, 2, 3] [4, 5, 6]
+
+z :: Maybe Integer
+z = lookup 2 $ zip [1, 2, 3] [4, 5, 6]
+
+tupled :: Maybe (Integer, Integer)
+tupled = (,) <$> y <*> z
+
+-- 3
+x :: Maybe Int
+x = elemIndex 3 ([1, 2, 3, 4, 5] :: [Int])
+
+y' :: Maybe Int
+y' = elemIndex 4 [1, 2, 3, 4, 5]
+
+max' :: Int -> Int -> Int
+max' = max
+
+maxed :: Maybe Int
+maxed = max' <$> x <*> y'
+
+-- 4
+xs :: [Integer]
+xs = [1, 2, 3]
+
+ys :: [Integer]
+ys = [4, 5, 6]
+
+x4 :: Maybe Integer
+x4 = lookup 3 $ zip xs ys
+
+y4 :: Maybe Integer
+y4 = lookup 2 $ zip xs ys
+
+summed :: Maybe Integer
+summed = sum <$> ((,) <$> x4 <*> y4)
+
