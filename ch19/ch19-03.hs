17f3e9cde12ea5080bbd5e97694d545df5bab6c6
danhlutz <danhlutz@gmail.com>
2018-11-19 17:35:03 -0500

    Write a utility to combine two functions

---
+++ ch19/ch19-03.hs
@@ -0,0 +1,10 @@
+-- ch19-03.hs
+
+module Web.Shipping.Utils ((<||>)) where
+
+import Control.Applicative (liftA2)
+
+(<||>) :: (a -> Bool)
+       -> (a -> Bool)
+       -> a -> Bool
+(<||>) = liftA2 (||)
