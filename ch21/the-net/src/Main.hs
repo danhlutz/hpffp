50d8175fe8c981061d3c01381344262fd00f7676
danhlutz <danhlutz@gmail.com>
2018-11-23 09:49:55 -0500

    Traverse a list of URLs

--- ch21/the-net/src/Main.hs
+++ ch21/the-net/src/Main.hs
@@ -3,6 +3,16 @@ module Main where
 import Data.ByteString.Lazy hiding (map)
 import Network.Wreq
 
+urls :: [String]
+urls = [ "http://httpbin.org/ip"
+       , "http://httpbin.org/bytes/5"]
+
+mappingGet :: [IO (Response ByteString)]
+mappingGet = map get urls
+
+traversedUrls :: IO [Response ByteString]
+traversedUrls = traverse get urls
+
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  Prelude.putStrLn "testing"
