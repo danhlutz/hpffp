ae1e77e65195b6545fbd1740bb5a81169b45c5a7
danhlutz <danhlutz@gmail.com>
2018-12-02 09:45:24 -0500

    Complete practice problems

---
+++ ch22/ex06.hs
@@ -0,0 +1,76 @@
+-- ch22/ex06.hs
+
+module Exercise where
+
+import Control.Applicative
+import Data.Maybe
+
+x :: [Integer]
+x = [1, 2, 3]
+
+y :: [Integer]
+y = [4, 5, 6]
+
+z :: [Integer]
+z = [7, 8, 9]
+
+lookup' :: Eq a => a -> [(a, b)] -> Maybe b
+lookup' _ [] = Nothing
+lookup' m ((i, v):ns) =
+  if m == i
+  then Just v
+  else lookup' m ns
+
+xs :: Maybe Integer
+xs = lookup' 3 $ zip x y
+
+ys :: Maybe Integer
+ys = lookup' 6 $ zip y z
+
+zs :: Maybe Integer
+zs = lookup' 4 $ zip x y
+
+z' :: Integer -> Maybe Integer
+z' n = lookup' n $ zip x z
+
+x1 :: Maybe (Integer, Integer)
+x1 = (,) <$> xs <*> ys
+
+x2 :: Maybe (Integer, Integer)
+x2 = (,) <$> ys <*> zs
+
+x3 :: Integer -> (Maybe Integer, Maybe Integer)
+x3 n = (,) (z' n) (z' n)
+
+uncurry :: (a -> b -> c) -> (a, b) -> c
+uncurry f m = f (fst m) (snd m)
+
+summed :: Num c => (c, c) -> c
+summed m = (+) (fst m) (snd m)
+
+s' :: Maybe Integer
+s' = summed <$> ((,) <$> xs <*> ys)
+
+bolt :: Integer -> Bool
+bolt = (&&) <$>  (>3) <*> (<8)
+
+sequA :: Integral a => a -> [Bool]
+sequA m = sequenceA [(>3), (<8), even] m
+
+main :: IO ()
+main = do
+  print $ sequenceA [Just 3, Just 2, Just 1]
+  print $ sequenceA [x, y]
+  print $ sequenceA [xs, ys]
+  print $ summed <$> ((,) <$> xs <*> ys)
+  print $ fmap summed ((,) <$> xs <*> zs)
+  print $ bolt 7
+  print $ fmap bolt z
+  print $ sequenceA [(>3), (<8), even] 7
+  putStrLn "New stuff here"
+  -- 1
+  print $ foldr (&&) True (sequA 14)
+  -- 2
+  print $ sequA (fromMaybe 0 s')
+  -- 3
+  print $ bolt (fromMaybe 0 ys) 
