68ce6626d6630d71c56a5a3bd39ec06c18df3e12
danhlutz <danhlutz@gmail.com>
2018-12-03 17:52:21 -0500

    Roll the dice!

---
+++ ch23/ex01.hs
@@ -0,0 +1,96 @@
+-- ch23-02.hs
+
+module Practice where
+
+import Control.Applicative (liftA3)
+import Control.Monad (replicateM)
+import Control.Monad.Trans.State
+import System.Random
+
+data Die =
+    DieOne
+  | DieTwo
+  | DieThree
+  | DieFour
+  | DieFive
+  | DieSix
+  deriving (Eq, Show)
+
+intToDie :: Int -> Die
+intToDie n =
+  case n of
+    1 -> DieOne
+    2 -> DieTwo
+    3 -> DieThree
+    4 -> DieFour
+    5 -> DieFive
+    6 -> DieSix
+    x -> error $
+      "intToDie got non 1-6 integer: " ++ show x
+
+rollDie :: State StdGen Die
+rollDie = state $ do
+  (n, s) <- randomR (1, 6)
+  return (intToDie n, s)
+
+rollDie' :: State StdGen Die
+rollDie' =
+  intToDie <$> state (randomR (1, 6))
+
+rollDieThreeTimes' :: State StdGen (Die, Die, Die)
+rollDieThreeTimes' =
+  liftA3 (,,) rollDie' rollDie' rollDie'
+
+infiniteDie :: State StdGen [Die]
+infiniteDie = repeat <$> rollDie'
+
+nDie :: Int -> State StdGen [Die]
+nDie n = replicateM n rollDie'
+
+rollsToGetTwenty :: StdGen -> Int
+rollsToGetTwenty g = go 0 0 g
+  where
+    go :: Int -> Int -> StdGen -> Int
+    go sum' count gen
+      | sum' >= 20 = count
+      | otherwise =
+        let (die, nextGen) =
+              randomR (1, 6) gen
+        in go (sum' + die)
+              (count + 1) nextGen
+
+-- exercises
+
+rollsToGetN :: Int -> StdGen -> Int
+rollsToGetN n g = go 0 0 g
+ where
+   go :: Int -> Int -> StdGen -> Int
+   go sum' count gen
+     | sum' >= n = count
+     | otherwise =
+       let (die, nextGen) =
+             randomR (1, 6) gen
+       in go (sum' + die)
+             (count + 1)
+             nextGen
+
+rollsCountLogged :: Int -> StdGen -> (Int, [Die])
+rollsCountLogged n g = go 0 (0, []) g
+  where
+    go :: Int -> (Int, [Die]) -> StdGen -> (Int, [Die])
+    go sum' logged gen
+      | sum' >= n = logged
+      | otherwise =
+        let (die, nextGen) =
+              randomR (1, 6) gen
+        in go (sum' + die)
+              (logRoll die logged)
+              nextGen
+
+logRoll :: Int -> (Int, [Die]) -> (Int, [Die])
+logRoll die logged =
+  (count + 1, intToDie die : rolls)
+  where count :: Int
+        count = fst logged
+        rolls :: [Die]
+        rolls = snd logged
