f88198715ea0f16ee171b5ce2ae2c41da2e98cc1
danhlutz <danhlutz@gmail.com>
2019-01-07 09:14:23 -0500

    Try to wrap up a ReaderT monad, but can't

---
+++ ch26/ex03.hs
@@ -0,0 +1,26 @@
+-- ch26-03.hs
+
+module Practice where
+
+import Control.Monad.Trans.Except
+import Control.Monad.Trans.Maybe
+import Control.Monad.Trans.Reader
+
+embedded :: MaybeT
+            (ExceptT String
+                     (ReaderT () IO))
+            Int
+embedded = _ (const (Right (Just 1)))
+
+maybeUnwrap :: ExceptT String
+               (ReaderT () IO) (Maybe Int)
+maybeUnwrap = runMaybeT embedded
+
+eitherUnwrap :: ReaderT () IO
+                (Either String (Maybe Int))
+eitherUnwrap = runExceptT maybeUnwrap
+
+readerUnwrap :: ()
+             -> IO (Either String
+                           (Maybe Int))
+readerUnwrap = runReaderT eitherUnwrap
