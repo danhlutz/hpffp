ca10ddecb0405e49c13285b7f71bc10779f3866b
danhlutz <danhlutz@gmail.com>
2019-01-08 17:44:16 -0500

    Rewrite scotty application using MonadIO

---
+++ ch26/scotty2.hs
@@ -0,0 +1,20 @@
+-- scotty2.hs
+
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Web.Scotty
+
+import Control.Monad.IO.Class
+import Data.Monoid (mconcat)
+
+main :: IO ()
+main = scotty 3000 $ do
+  get "/:word" $ do
+    beam <- param "word"
+    liftIO (putStrLn "hello")
+    html $
+      mconcat ["<h1>Scotty, " ,
+               beam,
+               " me up!</h1>"]
