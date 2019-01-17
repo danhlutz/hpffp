4af680be1275c5778151005f5e341245454cd673
danhlutz <danhlutz@gmail.com>
2019-01-17 17:59:48 -0500

    Benchmark a list indexing function

---
+++ ch28/bench.hs
@@ -0,0 +1,21 @@
+-- ch28/bench.hs
+
+module Main where
+
+import Criterion.Main
+
+infixl 9 !?
+_      !? n | n <0 = Nothing
+[]     !? _        = Nothing
+(x:_)  !? 0        = Just x
+(_:xs) !? n        = xs !? (n-1)
+
+myList :: [Int]
+myList = [1..9999]
+
+main :: IO ()
+main = defaultMain
+  [ bench "index list 9999"
+    $ whnf (myList !!) 9998
+  , bench "Index list maybe index 9999"
+    $ whnf (myList !?) 9998]
