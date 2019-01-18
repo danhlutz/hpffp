1a0ee0e7d16313a1ce735fd65b4613e6e872deb0
danhlutz <danhlutz@gmail.com>
2019-01-17 21:29:19 -0500

    Improve indexing function

--- ch28/bench.hs
+++ ch28/bench.hs
@@ -5,10 +5,17 @@ module Main where
 import Criterion.Main
 
 infixl 9 !?
-_      !? n | n <0 = Nothing
-[]     !? _        = Nothing
-(x:_)  !? 0        = Just x
-(_:xs) !? n        = xs !? (n-1)
+{-# INLINABLE (!?) #-}
+(!?) :: [a] -> Int -> Maybe a
+xs !? n
+  | n <0       = Nothing
+  | otherwise =
+    foldr
+    (\x r k ->
+       case k of
+         0 -> Just x
+         _ -> r (k-1))
+    (const Nothing) xs n
 
 myList :: [Int]
 myList = [1..9999]
