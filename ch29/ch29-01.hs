b83c289e0db2471f14236036bed19939370c4677
danhlutz <danhlutz@gmail.com>
2019-01-28 07:55:55 -0500

    See how IO can disable sharing

---
+++ ch29/ch29-01.hs
@@ -0,0 +1,16 @@
+-- ch29-01.hs
+
+module Practice where
+
+import Control.Concurrent
+
+myData :: IO (MVar Int)
+myData = newEmptyMVar
+
+main :: IO ()
+main = do
+  mv <- myData
+  putMVar mv 0
+  mv' <- myData
+  zero <- takeMVar mv'
+  print zero
