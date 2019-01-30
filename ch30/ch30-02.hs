c3fe5b43b3aaf25cfb7d485c2264ee696bb34f83
danhlutz <danhlutz@gmail.com>
2019-01-30 17:37:34 -0500

    Handle exception by writing to another file

--- ch30/ch30-02.hs
+++ ch30/ch30-02.hs
@@ -7,10 +7,12 @@ import Data.Typeable
 
 handler :: SomeException -> IO ()
 handler (SomeException e) = do
-  print (typeOf e)
-  putStrLn $ "We errored! It was" ++ show e
+  putStrLn ("Running main cause an error!\
+            \ It was: "
+           ++ show e)
+  writeFile "bbb" "hi"
 
 main :: IO ()
 main = do
-  writeFile "aaa" "hi"
+  writeFile "zzz" "hi"
     `catch` handler
