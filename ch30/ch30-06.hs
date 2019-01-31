b9dc92d888a06844587f1988ed55d67d8722ee0a
danhlutz <danhlutz@gmail.com>
2019-01-31 08:37:00 -0500

    Catch exceptions

--- ch30/ch30-06.hs
+++ ch30/ch30-06.hs
@@ -21,3 +21,21 @@ evenAndThreeDiv i
   | rem i 3 /= 0 = throwIO (NotDivThree i)
   | odd i     = throwIO (NotEven i)
   | otherwise = return i
+
+catchNotDivThree :: IO Int
+                 -> (NotDivThree -> IO Int)
+                 -> IO Int
+catchNotDivThree = catch
+
+catchNotEven :: IO Int
+             -> (NotEven -> IO Int)
+             -> IO Int
+catchNotEven = catch
+
+catchBoth ioInt =
+  catches ioInt
+  [ Handler
+      (\(NotEven _) -> return maxBound)
+  , Handler
+      (\(NotDivThree _) -> return minBound)
+  ]
