15d2a6734ae821087c3c9142bde1f730515d8e02
danhlutz <danhlutz@gmail.com>
2019-01-31 08:31:09 -0500

    Make our own exceptions

---
+++ ch30/ch30-06.hs
@@ -0,0 +1,23 @@
+-- ch30-06.hs
+
+module Practice where
+
+import Control.Exception
+
+data NotDivThree =
+  NotDivThree Int
+  deriving (Eq, Show)
+
+instance Exception NotDivThree
+
+data NotEven =
+  NotEven Int
+  deriving (Eq, Show)
+
+instance Exception NotEven
+
+evenAndThreeDiv :: Int -> IO Int
+evenAndThreeDiv i
+  | rem i 3 /= 0 = throwIO (NotDivThree i)
+  | odd i     = throwIO (NotEven i)
+  | otherwise = return i
